f13f71bbe405c5cd6ef2eb2090d7012f0df53e93
Marla Marcianelli <marlavous@users.noreply.github.com>
2016-08-25 12:13:24 -0700

    Add files via upload

---
+++ finalproject.py
@@ -0,0 +1,68 @@
+ ''' this game will help you decide what to have for dinner.'''
+
+def music():
+ 	#what music should be playing while you eat?
+ 	# 1 - Stranger Things soundtrack
+ 	# 2 - Born in the USA by Bruce Springstein
+ 	# 3 - The Schulyer Sisters by Hamilton Broadway Cast
+ 	# 4 - Hotline Bling by Drake
+ 	# 5 - Rolling in the Deep by Adele
+
+def eatstyle():
+	#How would you describe your eating style?
+	#1 - normal...whatever that is?
+	#2 - bird-like
+	#3 - wild animal
+	#4 - sophiscated
+
+def spice():
+	#"1 - not hot at all"
+	#"2 - ooh thats got a little kick!"
+	#"3 - my mouth is on fire!"
+	#"4 - I'm happy with whatever happens"
+
+def time():
+	#when did you last eat?
+	# 1 - I had a sensible, healthy lunch
+	# 2 - I'm so hangry I cant remember!
+	# 3 - I can always eat, feed me please
+	# 4 - I base my meals on emotions not hunger
+
+def drink():
+	#whats your favorite cocktail to have before dinner?
+	# 1 - gin martini stirred with a twist
+	# 2 - mojito
+	# 3 - a beer with a shot of bourbon on the side
+	# 4 - margarita
+	# 5 - a glass of wine
+
+def utensils():
+	#what kind of utensils do you want to use?
+	# 1 - a steak knife
+	# 2 - your hands
+	# 3 - chopsticks
+	# 4 - umm, a fork duh
+
+def style():
+	#all foods should really be what:
+	# 1 - healthy
+	# 2 - farm to table artistic creations
+	# 3 - dessert
+	# 4 - deep-fried
+	# 5 - spicy with many condiments
+
+def final_answer():
+	#Are you going to cook or get something delivered?
+	# 1 - cook
+	# 2 - delivery
+
+	#need to research recipes/delivery options to link to as a final answer
+
+
+def main():
+ 	pass
+
+
+
+if __name__ == '__main__':
+ 	main()
